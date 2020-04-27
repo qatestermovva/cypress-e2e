3adfb92fb991aeb94013df227a17c7ba8ffc7b53
qatester.movva <qatester.movva@gmail.com>
2020-04-27 14:50:36 +0200

    selecting the textbox elements through unique ids

--- cypress/integration/selecting.spec.js
+++ cypress/integration/selecting.spec.js
@@ -2,22 +2,19 @@ describe('Text box with max characters', ()=>{
     it('displays the appropriate remaining characters count', () => {
         cy.visit('http://localhost:3000/example-3')
 
-        cy.get('span')
-            .eq(1)
+        cy.get('[data-cy= "last-name-chars-left-count"]')
             .invoke('text')
             .should('equal', '15');
 
-        cy.get('input').eq(1).type('hello');
+        cy.get('[data-cy="input-last-name"]').type('hello');
 
-        cy.get('span')
-            .eq(1)
+        cy.get('[data-cy= "last-name-chars-left-count"]')
             .invoke('text')
             .should('equal', '10');
 
-        cy.get('input').eq(1).type(' my friend');
+        cy.get('[data-cy="input-last-name"]').type(' my friend');
 
-        cy.get('span')
-            .eq(1)
+        cy.get('[data-cy= "last-name-chars-left-count"]')
             .invoke('text')
             .should('equal', '0');
     });
@@ -25,9 +22,9 @@ describe('Text box with max characters', ()=>{
     it('prevents the user from typing more characters once max is exceeded', () => {
         cy.visit('http://localhost:3000/example-3');
 
-        cy.get('input').type('abcdefghijklmnopqrstuvwxyz');
+        cy.get('[data-cy="input-last-name"]').type('abcdefghijklmnopqrstuvwxyz');
 
-        cy.get('input')
+        cy.get('[data-cy="input-last-name"]')
             .should('have.attr', 'value', 'abcdefghijklmno');
     });
 });
